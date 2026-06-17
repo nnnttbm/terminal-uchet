1c98bcffec1e2c6809906d47412d4988b9648a62
nnnttbm <2nw2ydg8p8@privaterelay.appleid.com>
2026-06-17 14:55:23 +0300

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -420,7 +420,7 @@ export default function App() {
   <AlertCircle className={`relative w-4 h-4 z-10 ${activeTab === 'warranty' ? 'text-blue-300' : 'text-slate-400'}`} />
   <span className="relative z-10">{t('Гарнатія')}</span>
                   
-                  <button
+                  </button>
                     onClick={() => setActiveTab('help')}
                     className="relative flex items-center space-x-2 px-5 py-2.5 rounded-xl text-xs font-bold tracking-wide transition-colors cursor-pointer"
                   >
